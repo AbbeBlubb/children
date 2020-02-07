bab5cc8cb5ed9a9f0be04c0fb9daca1972a22d20
Alberto Chávez Pérez <alberto@francisco.nu>
2020-02-08 00:14:52 +0100

    Search for component name and retrieve key/index

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Children names
       </header>
+
+      <Parent>
+
+        <p>
+          ChildContainer order in JSX: One, Two, Three, Class<br/>
+          Parent renders in other order<br/>
+          This is a child as well, a /P/
+        </p>
+
+        <ChildContainerOne>
+          First child content
+        </ChildContainerOne>
+
+        <ChildContainerTwo>
+          Second child content
+        </ChildContainerTwo>
+
+        <ChildContainerThree>
+          Third child content
+        </ChildContainerThree>
+
+        <ClassChildContainer>
+          Content to class component
+        </ClassChildContainer>
+
+      </Parent>
+
     </div>
   );
-}
+};
 
-export default App;
+export class Parent extends React.Component {
+
+  getComponentName = name => {
+    console.log(this.props.children)
+    console.log(React.Children.toArray(this.props.children))
+    
+    let b = false;
+
+    React.Children.forEach(React.Children.toArray(this.props.children), child => {
+      console.log('Child object: ', child);
+
+      if((typeof child.type === 'function') && (child.type.name === name)){
+        console.log('Funcion (React Component) name: ', child.type.name);
+        const a = child.key;
+        console.log('return: ', a.substring(1))
+        b = a.substring(1)
+        return b
+      }
+    });
+    return b
+  };
+
+  render() {
+    return(
+      <div>
+        {this.props.children[1]}
+        {
+          this.getComponentName('ChildContainerThree')
+            ? this.props.children[this.getComponentName('ChildContainerThree')]
+            : 'hm'
+        }
+      </div>
+    );
+  }
+};
+
+export const ChildContainerOne = props => {
+  return(
+    <div className="child-container-one">
+      {props.children}
+    </div>
+  );
+};
+
+export const ChildContainerTwo = props => {
+  return(
+    <div className="child-container-two">
+      {props.children}
+    </div>
+  );
+};
+
+export const ChildContainerThree = props => {
+  return(
+    <div className="child-container-three">
+      {props.children}
+    </div>
+  );
+};
+
+export class ClassChildContainer extends React.Component {
+
+  render() {
+    return(
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
